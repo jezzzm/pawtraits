e6453b315986bfd49de639152ad40b77d6a98747
Jeremy Milledge <jez.milledge@gmail.com>
2020-06-13 16:01:56 +1000

    also read process.env for contentful keys

--- contentful-config.js
+++ contentful-config.js
@@ -3,9 +3,12 @@ const dotenv = require('dotenv');
 const { parsed } = dotenv.config({ path: '.env' });
 
 const options = {
-  spaceId: parsed.CONTENTFUL_SPACE_ID,
-  accessToken: parsed.CONTENTFUL_ACCESS_TOKEN,
-  managementToken: parsed.CONTENTFUL_MANAGEMENT_TOKEN,
+  spaceId: parsed.CONTENTFUL_SPACE_ID || process.env.CONTENTFUL_SPACE_ID,
+  accessToken:
+    parsed.CONTENTFUL_ACCESS_TOKEN || process.env.CONTENTFUL_ACCESS_TOKEN,
+  managementToken:
+    parsed.CONTENTFUL_MANAGEMENT_TOKEN ||
+    process.env.CONTENTFUL_MANAGEMENT_TOKEN,
 };
 
 const { spaceId, accessToken } = options;
