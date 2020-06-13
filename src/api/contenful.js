6fb07546dae8986f1d178d9bfe22772e9d6c05d1
Jeremy Milledge <jez.milledge@gmail.com>
2020-06-13 17:23:13 +1000

    missing env varss

--- src/api/contenful.js
+++ src/api/contenful.js
@@ -1,8 +1,8 @@
 import * as contentful from 'contentful-management';
 
 const client = contentful.createClient({
-  accessToken: process.env.CONTENTFUL_MANAGEMENT_TOKEN,
-})
+  accessToken: process.env.GATSBY_CONTENTFUL_MANAGEMENT_TOKEN,
+});
 
 export const requestPawtrait = async ({
   requesterName,
@@ -14,46 +14,46 @@ export const requestPawtrait = async ({
   referenceImages,
   rushed,
   extraPrints,
-  additionalComments
+  additionalComments,
 }) => {
   try {
-    const space = await client.getSpace(process.env.CONTENTFUL_SPACE_ID)
+    const space = await client.getSpace(process.env.GATSBY_CONTENTFUL_SPACE_ID);
     const post = await space.createEntry('pawtraitRequest', {
       fields: {
         requesterName: {
-          'en-US': requesterName
+          'en-US': requesterName,
         },
         requesterEmail: {
-          'en-US': requesterEmail
+          'en-US': requesterEmail,
         },
         requesterPhone: {
-          'en-US': requesterPhone
+          'en-US': requesterPhone,
         },
         petName: {
-          'en-US': petName
+          'en-US': petName,
         },
         breed: {
-          'en-US': breed
+          'en-US': breed,
         },
         description: {
-          'en-US': description
+          'en-US': description,
         },
         referenceImages: {
-          'en-US': referenceImages
+          'en-US': referenceImages,
         },
         rushed: {
-          'en-US': rushed
+          'en-US': rushed,
         },
         extraPrints: {
-          'en-US': extraPrints
+          'en-US': extraPrints,
         },
         additionalComments: {
-          'en-US': additionalComments
+          'en-US': additionalComments,
         },
-      }
-    })
-    console.log('succeeded: ', post)
+      },
+    });
+    console.log('succeeded: ', post);
   } catch (err) {
-    console.log('failed :', err)
+    console.log('failed :', err);
   }
-}
+};
