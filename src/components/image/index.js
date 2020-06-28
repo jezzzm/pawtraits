0727192efebfa86d31686fc0461b8940bf39d522
Jeremy Milledge <jez.milledge@gmail.com>
2020-06-28 15:37:39 +1000

    no ssr

--- src/components/image/index.js
+++ src/components/image/index.js
@@ -1,37 +1,25 @@
-import React, { useCallback, useRef } from 'react';
-import QuickPinchZoom, { make3dTransformValue } from 'react-quick-pinch-zoom';
+import React from 'react';
 import * as styles from './image.style';
+import useWindowSize from '../../utils/use-window-size';
 
 export default function Image({ name, alt, src, srcSet, srcSetWebp, sizes }) {
-  const imgRef = useRef();
-  const onUpdate = useCallback(({ x, y, scale }) => {
-    const { current: img } = imgRef;
-
-    if (img) {
-      const value = make3dTransformValue({ x, y, scale });
-
-      img.style.setProperty('transform', value);
-    }
-  }, []);
+  const { isMobile } = useWindowSize();
   return (
     <>
       <div css={styles.info}>
         <h1>{name}</h1>
       </div>
       <div css={styles.imageContainer}>
-        <QuickPinchZoom onUpdate={onUpdate}>
-          <picture css={styles.image}>
-            <source srcSet={srcSetWebp} sizes={sizes} type="image/webp" />
-            <source srcSet={srcSet} sizes={sizes} type="image/png" />
-            <img
-              ref={imgRef}
-              alt={alt}
-              src={src}
-              loading="lazy"
-              draggable
-            />
-          </picture>
-        </QuickPinchZoom>
+        <picture css={styles.image}>
+          <source srcSet={srcSetWebp} sizes={sizes} type="image/webp" />
+          <source srcSet={srcSet} sizes={sizes} type="image/png" />
+          <img
+            alt={alt}
+            src={src}
+            loading="lazy"
+            draggable={isMobile}
+          />
+        </picture>
       </div>
     </>
   );
